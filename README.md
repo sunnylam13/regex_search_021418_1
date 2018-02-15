0ef459768ee704a62dba069d95c9c1a12db51ab4
Sunny Lam <sunny.lam@gmail.com>
2018-02-15 11:48:15 -0500

    revised doc and code

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ This opens all `.txt` files in a folder and searches for any line that matches a
 
 The results are printed to the terminal screen.
 
+## Test Data
+
+The test data folder path is:
+
+docs/testTxtFolder
+
+(this is relative)
+
 ## References
 
 ABSP: 319
--- regex_search/regex_search_021418_1.py
+++ regex_search/regex_search_021418_1.py
@@ -2,9 +2,20 @@
 
 # ! /usr/local/Cellar/python3/3.6.1
 
+import re, os
+
 # get user supplied folder or folder path
+
+folder_path_input = input("Please enter the folder or folder path of the text files you wish to search with a regex expression.\n")
+
 # get user supplied regular expression
+
+regex_input = input("Please enter the regex expression you want to search for:\n")
+
 # get a list of all the `.txt` files in the folder
+
+file_list = os.listdir(folder_path_input) # return list of filename strings for each file in the `path` arg
+
 # TODO:  code a way to search deeper into any additional folders for `.txt` files
 # print results to the terminal screen
 
