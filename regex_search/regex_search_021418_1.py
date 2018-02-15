4eaa850a6bf2cf64fe882821255c90910ba7c10d
Sunny Lam <sunny.lam@gmail.com>
2018-02-15 18:33:32 -0500

    revised code

--- regex_search/regex_search_021418_1.py
+++ regex_search/regex_search_021418_1.py
@@ -149,10 +149,10 @@ def analyze_file_folders(file_path_list):
 			print("It seems you've run into an error.")
 			break # break the loop here
 
-	# then run analyze_file_folders() again
-	# there should be some sort of conditional loop to keep analyzing `searchable_folder_list` until it's exhausted
+	# # then run analyze_file_folders() again
+	# # there should be some sort of conditional loop to keep analyzing `searchable_folder_list` until it's exhausted
 
-	analyze_file_folders(searchable_folder_list)
+	# analyze_file_folders(searchable_folder_list)
 
 def analyze_file_for_textFile(item_file_path):
 	# a function that analyzes whether said file is a text file and then store it in `txt_file_list`
@@ -190,7 +190,11 @@ analyze_file_folders(file_list_fullPath_final)
 # then run analyze_file_folders() again
 # there should be some sort of conditional loop to keep analyzing `searchable_folder_list` until it's exhausted
 
-analyze_file_folders(searchable_folder_list)
+# analyze_file_folders(searchable_folder_list)
+
+# if more folders are appended as the loop progresses, it only stops when no more folders have been appended to `searchable_folder_list`
+for folder in searchable_folder_list:
+	analyze_file_folders(folder)
 
 # EXPECT:  ['../docs/testTxtFolder/doc1.txt','../docs/testTxtFolder/doc2.txt','../docs/testTxtFolder/doc3.txt','../docs/testTxtFolder/doc4.txt','../docs/testTxtFolder/doc5.txt']
 
