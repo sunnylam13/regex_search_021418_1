5168179e129382cda37739798876a985512bc210
Sunny Lam <sunny.lam@gmail.com>
2018-02-15 12:07:58 -0500

    revised code

--- regex_search/regex_search_021418_1.py
+++ regex_search/regex_search_021418_1.py
@@ -30,18 +30,41 @@ regex_input = input("Please enter the regex expression you want to search for:\n
 #####################################
 
 # get a list of all the `.txt` files in the folder
+
+# TODO:  code a way to search deeper into any additional folders for `.txt` files
+
 # EXPECTED OUTPUT:
 # ['docFolderL1', 'doc1.txt', 'doc3.txt', 'doc2.txt']
 
-file_list = os.listdir(folder_path_input) # return list of filename strings for each file in the `path` arg
+file_list_processing = os.listdir(folder_path_input) # return list of filename strings for each file in the `path` arg
 
-print(file_list)
+print(file_list_processing) # for testing
+
+# check each item in file_list_processing to see if it's a folder
+# if it is, go deeper into it and get text files
+# this may be a function that recursively calls itself as you can have nested folders
+# add each text file to list `file_list_final`
+
+searchable_folder_list = [] # should be the string path of each folder
+
+txt_file_list = [] # should be the string path of each text file
+
+def folder_finder(folder_path_input):
+	initial_folder = os.listdir(folder_path_input) # gets list of contents within the initially supplied folder
+	# loop through the contents and check if it is a folder
+	# if it's a folder, add it to the `searchable_folder_list`
+
+def file_finder(folder_path_input):
+	pass
+
+def add_txt_to_file_list_final(text_file):
+	pass
+
+# cycle through folder list to find sub-directories
 
 #####################################
 # END FOLDER ANALYSIS, FIND TEXT FILES
 #####################################
 
-
-# TODO:  code a way to search deeper into any additional folders for `.txt` files
 # print results to the terminal screen
 
