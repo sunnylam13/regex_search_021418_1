58241430e7b0965edfe4869f9e764ae2c3386672
Sunny Lam <sunny.lam@gmail.com>
2018-02-14 19:40:16 -0500

    setup the outline

---
+++ regex_search/regex_search_021418_1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# ! /usr/local/Cellar/python3/3.6.1
+
+# get user supplied folder or folder path
+# get user supplied regular expression
+# get a list of all the `.txt` files in the folder
+# TODO:  code a way to search deeper into any additional folders for `.txt` files
+# print results to the terminal screen
+
